f8a1df1ceb2c0105e09b2c33172173e77ac08a90
Gregor Richards <hg-yff@gregor.im>
2021-06-07 23:20:42 -0400

    Updating the old test runner for the new way.

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 "strict mode";
 const fs = require("fs");
-const eyc = require("./eyc-dbg.js").eyc();
-
-eyc.ext.fetch = async function(ignore, path) {
-    return fs.readFileSync(path, "utf8");
-};
+const EYC = require("./eyc-dbg.js");
 
 (async function() {
+
+    const eyc = await EYC.eyc();
+    eyc.ext.fetch = async function(ignore, path) {
+        return fs.readFileSync(path, "utf8");
+    };
+
     let emodule = await eyc.importModule(process.argv[2], {text: fs.readFileSync(process.argv[2], "utf8"), ctx: {privileged: true}});
     let x = new eyc.Object();
     x.extend(emodule.main.klass.prefix);
     x.methods.$$core$Stage$init(eyc, x, eyc.nil);
-    return;
     for (let i = 0; i < 1024; i++) {
         let a = process.hrtime();
         x.methods.$$core$Stage$tick(eyc, x, eyc.nil);
