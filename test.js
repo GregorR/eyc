000856d241ccca6a562e374208ac2dffc5cbb0b2
Gregor Richards <hg-yff@gregor.im>
2022-09-17 18:24:36 -0400

    Adding more documentation and a different execution mode.

--- test.js
+++ test.js
@@ -12,13 +12,18 @@ const EYC = require("./eyc-dbg.js");
     let emodule = await eyc.importModule("/" + process.argv[2], {ctx: {privileged: true}});
     let x = new eyc.Object();
     x.extend(emodule.main.klass.prefix);
-    x.methods.$$core$Stage$init(eyc, x, eyc.nil);
-    for (let i = 0; i < 1024; i++) {
-        //let a = process.hrtime();
-        x.methods.$$core$Stage$tick(eyc, x, eyc.nil);
-        /*
-        let b = process.hrtime(a);
-        console.log(b);
-        */
+    x.methods.$$core$Program$init(eyc, x, eyc.nil);
+    if (x.methods.$$core$Stage$tick) {
+        for (let i = 0; i < 1024; i++) {
+            //let a = process.hrtime();
+            x.methods.$$core$Stage$tick(eyc, x, eyc.nil);
+            /*
+            let b = process.hrtime(a);
+            console.log(b);
+            */
+        }
+    } else {
+        // Should be a Basic program
+        x.methods.$$core$Basic$main(eyc, x, eyc.nil);
     }
 })();
